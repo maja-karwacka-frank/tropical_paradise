176de8c53990a59921f5368bf5e3fc7401236dec
maja-karwacka-frank <115737913+maja-karwacka-frank@users.noreply.github.com>
2023-01-02 12:59:11 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active');
+
+	navBtnBars.classList.remove('black-bars-color');
+
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active');
+		});
+	});
+
+	navAnimation();
+};
+
+const navAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+
+	allNavItems.forEach((item) => {
+		item.addEventListener('click', deleteAnimation);
+	});
+};
+
+const deleteAnimation = () => {
+	allNavItems.forEach((item) => {
+		item.classList.remove('nav-items-animation');
+	});
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (
+			!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
